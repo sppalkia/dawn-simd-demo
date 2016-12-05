9f14483fc135634938dddcce32278cc1ec656fe5
Shoumik Palkar <shoumikpalkar@gmail.com>
2016-12-05 13:12:08 -0800

    Add compiler guards

--- demo1/simd_demo.c
+++ demo1/simd_demo.c
@@ -1,27 +1,24 @@
 
 #include <math.h>
 
-// 1. Fast Math (add -ffast-math)
-// 2. Uncomment the if statements
-// 3. Talk about aliasing.
-// 4. Fickleness
-// ..
+#define ADD_ONE                 0
+#define SUM_LARGE_FLOATS        0
+#define SUM_LARGE_INTS          0
+#define NESTED_IFS              0
+#define SQRT_EACH               0
+#define UPDATE                  0
+#define UPDATE_RESTRICT         0
 
-/** Takes a vector v and writes the square root of each element
- * in result (or the element itself if element is negative).
- */
-void sqrt_each(float *result, const float *v, unsigned length) {
+#if ADD_ONE
+void add_one(float *v, unsigned length) {
     for (int i = 0; i < length; i++) {
-        float val = v[i];
-        if (val >= 0.0f) {
-                result[i] = sqrtf(val);
-        } else {
-            result[i] = val;
-        }
+        v[i]++;
     }
 }
+#endif
 
 // Sums all the values in v which are greater than 100.
+#if SUM_LARGE_FLOATS
 float sum_large_floats(const float *v, unsigned length) {
     float result = 0;
     for (int i = 0; i < length; i++) {
@@ -32,8 +29,10 @@ float sum_large_floats(const float *v, unsigned length) {
     }
     return result;
 }
+#endif
 
 // Sums all the values in v which are greater than 100.
+#if SUM_LARGE_INTS
 int sum_large_integers(const int *v, unsigned length) {
     int result = 0;
     for (int i = 0; i < length; i++) {
@@ -44,16 +43,48 @@ int sum_large_integers(const int *v, unsigned length) {
     }
     return result;
 }
+#endif
+
+#if NESTED_IFS
+void nested_ifs(int *v, unsigned length) {
+    for (int i = 0; i < length; i++) {
+        if (v[i] > 10) {
+            if (v[i] < 20) {
+                v[i]++;
+            }
+        }
+    }
+}
+#endif
+
+/** Takes a vector v and writes the square root of each element
+ * in result (or the element itself if element is negative).
+ */
+#if SQRT_EACH
+void sqrt_each(float *result, const float *v, unsigned length) {
+    for (int i = 0; i < length; i++) {
+        float val = v[i];
+        if (val >= 0.0f) {
+                result[i] = sqrtf(val);
+        } else {
+            result[i] = val;
+        }
+    }
+}
+#endif
 
 // Compiler can't tell where v1 and v2 point!
 // For example, v2[i+1] can == v1[i]
+#if UPDATE
 void update(float *v1, float *v2, unsigned length) {
     for (int i = 0; i < length; i++) {
         v1[i] = 2 * v2[i];
     }
 }
+#endif
 
 // Restrict says that v1 and v2 won't point to the same underlying object.
+#if UPDATE_RESTRICT
 void update_restrict(
         float* restrict v1,
         float* restrict v2,
@@ -63,4 +94,4 @@ void update_restrict(
         v1[i] = 2 * v2[i];
     }
 }
-
+#endif

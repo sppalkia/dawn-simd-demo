f12a11b5addf28bfb145af80489777283b045ef7
Shoumik Palkar <shoumikpalkar@gmail.com>
2016-12-05 01:01:42 -0800

    Fixed issue

--- demo2/demo2.c
+++ demo2/demo2.c
@@ -8,10 +8,10 @@
 // Sums all the values in v which are greater than 100.
 double sum_large_floats(const float *v, unsigned length, unsigned trials) {
     double result = 0.0f;
-    for (int t = 0; t < trials; t++) {
-        for (int i = 0; i < length; i++) {
+    for (unsigned t = 0; t < trials; t++) {
+        for (unsigned i = 0; i < length; i++) {
             float val = v[i];
-            if (val >= 100.0f) {
+            if (val >= 10.0f) {
                 result += val;
             }
         }
@@ -24,29 +24,28 @@ double sum_large_floats_vectorized(const float *v, unsigned length, unsigned tri
 
     double result = 0.0f;
 
-    for (int t = 0; t < trials; t++) {
+    for (unsigned t = 0; t < trials; t++) {
 
-        __m128 cutoff = _mm_set1_ps(100);
+        __m128 cutoff = _mm_set1_ps(10.0f);
         __m128 sum = _mm_setzero_ps();
 
         float final[4] __attribute__((aligned(16)));
 
-        int i;
+        unsigned i;
 
         for (i = 0; i+4 <= length; i+=4) {
-            //printf("%f %f %f %f\n", v[i], v[i+1], v[i+2], v[i+3]);
-            __m128 data = _mm_load_ps(v + i);
+            __m128 data = _mm_loadu_ps(v + i);
             __m128 mask = _mm_cmpge_ps(data, cutoff);
             __m128 masked = _mm_and_ps(mask, data);
 
             // Update the sum.
-            sum = _mm_add_ps(masked, sum);
+            sum = _mm_add_ps(sum, masked);
         }
 
         // Handle the fringe case.
         for (; i < length; i++) {
             float val = v[i];
-            if (val >= 100.0f) {
+            if (val >= 10.0f) {
                 result += val;
             }
         }
@@ -60,29 +59,29 @@ double sum_large_floats_vectorized(const float *v, unsigned length, unsigned tri
 }
 
 int main(int argc, char **argv) {
-    const unsigned LEN = 10000000;
+    const unsigned LEN = 1000000;
     float *v  = malloc(sizeof(float) * LEN);
 
     for (int i = 0; i < LEN; i++) {
-        v[i] = i+1;
+        v[i] = (float)((i+1) % 20);
     }
 
     struct timeval start, end, diff;
     double result;
 
     gettimeofday(&start, 0);
-    result = sum_large_floats(v, LEN, 1);
+    result = sum_large_floats(v, LEN, 100);
     gettimeofday(&end, 0);
     timersub(&end, &start, &diff);
     printf("Scalar: %ld.%06ld seconds (result=%f)\n",
             (long)diff.tv_sec, (long)diff.tv_usec,
-            (long)result);
+            result);
 
     gettimeofday(&start, 0);
-    result = sum_large_floats_vectorized(v, LEN, 1);
+    result = sum_large_floats_vectorized(v, LEN, 100);
     gettimeofday(&end, 0);
     timersub(&end, &start, &diff);
     printf("Vectorized: %ld.%06ld seconds (result=%f)\n",
             (long)diff.tv_sec, (long)diff.tv_usec,
-            (long)result);
+            result);
 }
